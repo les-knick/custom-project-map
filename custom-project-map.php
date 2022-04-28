aa69a4c7702bdb93addc2b6ec932761c54fc7dca
les-knick <lea.stocker@knickdesign.de>
2022-04-28 14:34:39 +0200

    add settings page in wp admin menu

--- custom-project-map.php
+++ custom-project-map.php
@@ -55,12 +55,45 @@ function cptui_register_my_cpts_projekt() {
 
 add_action( 'init', 'cptui_register_my_cpts_projekt' );
 
-add_action('admin_menu', array( $this, 'addPluginAdminMenu' ), 9);
-
-public function addPluginAdminMenu() {
-//add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-add_menu_page(  $this->plugin_name, 'Plugin Name', 'administrator', $this->plugin_name, array( $this, 'displayPluginAdminDashboard' ), 'dashicons-chart-area', 26 );
-
-//add_submenu_page( '$parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
-add_submenu_page( $this->plugin_name, 'Plugin Name Settings', 'Settings', 'administrator', $this->plugin_name.'-settings', array( $this, 'displayPluginAdminSettings' ));
+function dbi_add_settings_page() {
+    add_options_page( 'Custom Project Map Settings', 'Custom Project Map', 'manage_options', 'dbi-example-plugin', 'dbi_render_plugin_settings_page' );
 }
+
+function dbi_render_plugin_settings_page() {
+    ?>
+    <h2>Custom Project Map Settings</h2>
+    <form action="options.php" method="post">
+        <?php 
+        settings_fields( 'dbi_example_plugin_options' );
+        do_settings_sections( 'dbi_example_plugin' ); ?>
+        <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
+    </form>
+    <?php
+}
+
+function dbi_register_settings() {
+    register_setting( 'dbi_example_plugin_options', 'dbi_example_plugin_options', 'dbi_example_plugin_options_validate' );
+    add_settings_section( 'map_settings', 'Map Settings', 'dbi_plugin_section_text', 'dbi_example_plugin' );
+
+    add_settings_field( 'dbi_plugin_setting_api_key', 'API Key', 'dbi_plugin_setting_api_key', 'dbi_example_plugin', 'map_settings' );
+    add_settings_field( 'dbi_plugin_style_url', 'Style URL', 'dbi_plugin_style_url', 'dbi_example_plugin', 'map_settings' );
+}
+
+
+function dbi_plugin_section_text() {
+    echo '<p>Einstellungen für die Kartenanbindung</p>';
+}
+
+function dbi_plugin_setting_api_key() {
+    $options = get_option( 'dbi_example_plugin_options' );
+    echo "<input id='dbi_plugin_setting_api_key' name='dbi_example_plugin_options[api_key]' type='text' value='" . esc_attr( $options['api_key'] ) . "' />";
+}
+
+function dbi_plugin_style_url() {
+    $options = get_option( 'dbi_example_plugin_options' );
+    echo "<input id='dbi_plugin_style_url' name='dbi_example_plugin_options[style_url]' type='text' value='" . esc_attr( $options['style_url'] ) . "' />";
+}
+
+add_action( 'admin_init', 'dbi_register_settings' );
+add_action( 'admin_menu', 'dbi_add_settings_page' );
+?>
